108116770c92e1fb7e66aa78dc85d0c6c72df7cb
Neel Choudhary <neel.choudhary3@gmail.com>
2020-01-22 04:11:26 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 # Install AWS Command Line Interface
 
 # https://aws.amazon.com/cli/
-
 apk add --update python python-dev py-pip
 pip install awscli --upgrade
 
-docker pull $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME
+docker pull $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG
 
 # Set AWS config variables used during the AWS get-login command below
 
@@ -13,22 +12,28 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 # AWS login
-$(aws ecr get-login --no-include-email --region us-east-2)
+$(aws ecr get-login --no-include-email --region $AWS_REGION)
 
-# Build new docker image on go-lang-server ECR
-docker build -t go-lang-server .
+docker tag $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME $AWS_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
+docker push $AWS_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
 
 # Tag new docker image on go-lang-server ECR as latest
-docker tag go-lang-server:latest 729017073046.dkr.ecr.us-east-2.amazonaws.com/go-lang-server:latest
+# docker tag go-lang-server:latest 729017073046.dkr.ecr.us-east-2.amazonaws.com/go-lang-server:latest
 
 # Push new docker image to ECR
-docker push 729017073046.dkr.ecr.us-east-2.amazonaws.com/go-lang-server:latest
+# docker push 729017073046.dkr.ecr.us-east-2.amazonaws.com/go-lang-server:latest
+
+aws ecs register-task-definition --family boncuisine-$CI_ENVIRONMENT_SLUG-definition --requires-compatibilities FARGATE --cpu 256 --memory 512 --cli-input-json file://deploy-aws.json --region \$AWS_REGION
+
+# Tell our service to use the latest version of task definition.
+
+aws ecs update-service --cluster golang-cluster --service golang-container-prod-service --task-definition boncuisine-$CI_ENVIRONMENT_SLUG-definition --region \$AWS_REGION
 
 # Register New Task Definition
-aws ecs register-task-definition --family boncuisine-production-definition --requires-compatibilities FARGATE --cpu 256 --memory 512 --cli-input-json file://boncuisine-task-definition-production.json --region "us-east-2"
+# aws ecs register-task-definition --family boncuisine-production-definition --requires-compatibilities FARGATE --cpu 256 --memory 512 --cli-input-json file://boncuisine-task-definition-production.json --region "us-east-2"
 
 # Update service with new task and start task. This should end old task
-aws ecs update-service --cluster golang-cluster --service golang-container-prod-service --task-definition boncuisine-production-definition --region "us-east-2"
+# aws ecs update-service --cluster golang-cluster --service golang-container-prod-service --task-definition boncuisine-production-definition --region "us-east-2"
 
 # Done
 # docker tag $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_NAME $AWS_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
